1ee731182aff294cb33669f4d7da8093b1e60495
Andy Mihalik <andymihalik@gmail.com>
2022-06-18 10:41:42 -0500

    Playing around with Blazor

---
+++ FirstBlazorApp/FirstBlazorApp/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace FirstBlazorApp.Models
+{
+    internal class Employee : Entity
+    {
+        public string Position { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public Employee(int id, string name, string position, DateTime? startDate = null) : base(id, name)
+        {
+            Id = id;
+            Name = name;
+            Position = position;
+            StartDate = startDate.HasValue ? startDate.Value : DateTime.Today;
+        }
+    }
+}
---
+++ FirstBlazorApp/FirstBlazorApp/Models/Entity.cs
@@ -0,0 +1,16 @@
+﻿using FirstBlazorApp.Interfaces;
+
+namespace FirstBlazorApp.Models
+{
+    public class Entity : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Entity(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
